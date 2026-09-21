23267093c5fecefc062f7e1c275703d149f6c69a
chervas <car.hervas@gmail.com>
2026-09-21 07:37:15 +0000

    fix(agenda): retain review warning for inactive imported cabins

--- src/controllers/citas.controller.js
+++ src/controllers/citas.controller.js
@@ -2511,7 +2511,7 @@ exports.getCitasCalendar = asyncHandler(async (req, res) => {
                 model: Instalacion,
                 as: 'instalacion',
                 required: false,
-                attributes: ['id', 'nombre', 'color'],
+                attributes: ['id', 'nombre', 'color', 'activo'],
             },
             {
                 model: Tratamiento,
